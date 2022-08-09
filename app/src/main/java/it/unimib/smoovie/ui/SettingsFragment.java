06e0b08d55b8635e623b138f02c7c35167ef4144
Yang <j.xiang@campus.unimib.it>
2022-08-09 13:48:02 +0200

    Added Settings page

---
+++ app/src/main/java/it/unimib/smoovie/ui/SettingsFragment.java
@@ -0,0 +1,13 @@
+package it.unimib.smoovie.ui;
+
+import androidx.fragment.app.Fragment;
+
+import it.unimib.smoovie.R;
+
+public class SettingsFragment extends Fragment {
+
+    public SettingsFragment() {
+        super(R.layout.fragment_settings);
+    }
+
+}
