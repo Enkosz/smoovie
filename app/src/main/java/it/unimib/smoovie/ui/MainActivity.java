55cfcb41af624134159dd8530c6ecdb352610666
Yang <j.xiang@campus.unimib.it>
2022-08-12 13:42:15 +0200

    adding listener to switch button, line 46 doesn't run [need to fix]

--- app/src/main/java/it/unimib/smoovie/ui/MainActivity.java
+++ app/src/main/java/it/unimib/smoovie/ui/MainActivity.java
@@ -1,12 +1,19 @@
 package it.unimib.smoovie.ui;
+
+import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.util.Log;
 import android.view.View;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -18,7 +25,7 @@ import com.google.android.material.navigation.NavigationBarView;
 
 import it.unimib.smoovie.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +40,34 @@ public class MainActivity extends AppCompatActivity {
 
         // For the BottomNavigationView
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+        SwitchCompat switch_mature = findViewById(R.id.mature_content);
+
+        if (switch_mature != null) {
+            switch_mature.setOnCheckedChangeListener(this);
+            Log.i("sucapipo", "set!");
+        }
+//    switch_mature.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+////        @Override
+////        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+////            Log.v("Switch state", ""+isChecked);
+////        }
+//    });
+
+//        SharedPreferences settings = getSharedPreferences("CoolPreferences", 0);
+//        SharedPreferences.Editor editor = settings.edit();
+//        editor.putString("show_mature_content", "true");
+//        editor.commit();
+//
+//        String silent = settings.getString("show_mature_content", "value");
+//        Log.i("sucapipo", silent);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        Log.v("Switch state", "" + isChecked);
+        Log.i("sucapipo", "" + isChecked);
+        Toast.makeText(this, "The Switch is " + (isChecked ? "on" : "off"),
+                Toast.LENGTH_SHORT).show();
     }
 }
