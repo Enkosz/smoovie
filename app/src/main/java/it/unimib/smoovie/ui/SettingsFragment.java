d1e48b893a768cdbfc6c3f0ebdb3f410528e2201
Yang <j.xiang@campus.unimib.it>
2022-08-14 12:54:50 +0200

    fixed previous issue, completed Notifications section

--- app/src/main/java/it/unimib/smoovie/ui/SettingsFragment.java
+++ app/src/main/java/it/unimib/smoovie/ui/SettingsFragment.java
@@ -8,55 +8,57 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import it.unimib.smoovie.R;
 
 public class SettingsFragment extends Fragment {
-
-    private SwitchCompat switchMature;
-    private SwitchCompat switchStreamCellular;
-    private SharedPreferences sharedPreferences;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
 
-        sharedPreferences = getContext().getSharedPreferences("MySharedPref", Context.MODE_PRIVATE);
+        TextView textViewNotificationsSettings = view.findViewById(R.id.notifications_settings);
+        textViewNotificationsSettings.setOnClickListener(v -> Navigation.findNavController(v)
+                .navigate(R.id.notificationsFragment));
+
+        SharedPreferences sharedPreferences = getContext().getSharedPreferences("MySharedPref", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
 
-        switchMature = view.findViewById(R.id.mature_content);
-        switchMature.setChecked(sharedPreferences.getBoolean("showMature", false));
-        switchMature.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        SwitchCompat switchShowMatureContent = view.findViewById(R.id.switch_show_mature_content);
+        switchShowMatureContent.setChecked(sharedPreferences.getBoolean("showMatureContent", false));
+        switchShowMatureContent.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
                 if (isChecked) {
-                    editor.putBoolean("showMature", true);
-                    Log.i("mature", "true");
+                    editor.putBoolean("showMatureContent", true);
+                    Log.i("switch_show_mature_content", "true");
                 } else {
-                    editor.putBoolean("showMature", false);
-                    Log.i("mature", "false");
+                    editor.putBoolean("showMatureContent", false);
+                    Log.i("switch_show_mature_content", "false");
                 }
                 editor.apply();
             }
         });
 
-        switchStreamCellular = view.findViewById(R.id.stream_cellular);
-        switchStreamCellular.setChecked(sharedPreferences.getBoolean("streamCellular", false));
-        switchStreamCellular.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        SwitchCompat switchStreamUsingCellular = view.findViewById(R.id.switch_stream_using_cellular);
+        switchStreamUsingCellular.setChecked(sharedPreferences.getBoolean("streamUsingCellular", false));
+        switchStreamUsingCellular.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
                 if (isChecked) {
-                    editor.putBoolean("streamCellular", true);
-                    Log.i("cellular", "true");
+                    editor.putBoolean("streamUsingCellular", true);
+                    Log.i("stream_using_cellular", "true");
                 } else {
-                    editor.putBoolean("streamCellular", false);
-                    Log.i("cellular", "false");
+                    editor.putBoolean("streamUsingCellular", false);
+                    Log.i("stream_using_cellular", "false");
                 }
                 editor.apply();
             }
