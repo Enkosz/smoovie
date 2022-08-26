ab40536e96887f15fb51ff6324b4b1d93c181b51
yangxdev <j.xiang@campus.unimib.it>
2022-08-26 20:22:54 +0100

    [WIP] checkpoint

--- app/src/main/java/it/unimib/smoovie/ui/MainActivity.java
+++ app/src/main/java/it/unimib/smoovie/ui/MainActivity.java
@@ -11,6 +11,7 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import it.unimib.smoovie.R;
@@ -36,7 +37,8 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
 
         AtomicBoolean toReset = new AtomicBoolean(false);
-
+        AtomicBoolean inMovieDetail = new AtomicBoolean(false);
+        AtomicBoolean test = new AtomicBoolean(false);
         // Hide the bottom nav bar when we are in the login fragment
         navController.addOnDestinationChangedListener((ignored, navDestination, bundle) -> {
             if (navDestination.getId() == R.id.homeFragment) {
@@ -48,7 +50,6 @@ public class MainActivity extends AppCompatActivity {
                 toReset.set(false);
             }
             if (navDestination.getId() == R.id.resultsFragment) {
-//                navDestination.setId(R.id.searchFragment);
                 if (toReset.get()) {
                     Log.i("test", "setupNavControllerAuthenticationFilter: if, resetting to search");
                     Log.i("test", "setupNavControllerAuthenticationFilter: inside toReset = " + toReset.get());
@@ -62,19 +63,26 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
             if (navDestination.getId() == R.id.movieDetailFragment) {
-                toReset.set(false);
+                if (!inMovieDetail.get()) {
+                    toReset.set(false);
+                } else {
+                    inMovieDetail.set(false);
+                    test.set(true);
+                }
                 Log.i("test", "setupNavControllerAuthenticationFilter: movieDetailFragment");
                 if (toReset.get()) {
                     Log.i("test", "setupNavControllerAuthenticationFilter: if, resetting to search");
                     Log.i("test", "setupNavControllerAuthenticationFilter: inside toReset = " + toReset.get());
                     navController.navigate(R.id.searchFragment);
                     toReset.set(false);
-                }else {
+                } else {
                     Log.i("test", "setupNavControllerAuthenticationFilter: else, toReset = " + toReset.get());
                     Log.i("test", "setupNavControllerAuthenticationFilter: movieDetailFragment");
                     Log.i("test", "setupNavControllerAuthenticationFilter: setting toReset to true");
                     toReset.set(true);
+                    inMovieDetail.set(true);
                 }
+                Log.i("test", "setupNavControllerAuthenticationFilter: inMovieDetail = " + inMovieDetail.get());
             }
             if (navDestination.getId() == R.id.settingsFragment) {
                 Log.i("test", "setupNavControllerAuthenticationFilter: settingsFragment");
@@ -86,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
             else if (bottomNavigationView.getVisibility() != View.VISIBLE)
                 bottomNavigationView.setVisibility(View.VISIBLE);
 
-            Log.i("test", "----------");
+            Log.i("test", "setupNavControllerAuthenticationFilter: --------------------------------");
         });
     }
 }
